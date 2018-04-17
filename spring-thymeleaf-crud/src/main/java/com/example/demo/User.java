233b039889b1c2a45a8c8bccaea6bd62c7e4d95c
Yu0614 <azu369yu@gmail.com>
2018-04-17 12:13:48 +0900

    after fix of PleyerMapper.xml @2018/4/16

---
+++ spring-thymeleaf-crud/src/main/java/com/example/demo/User.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+
+public class User {
+	private int id;
+	private String name;
+
+	/*
+	 *  id setter&getter
+	 */
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/*
+	 *  name setter&getter
+	 */
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+
+	}
+
+
+
+
+}
